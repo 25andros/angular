047931773b15ec376ffc81b1f07e5247fb7efa45
andros25 <zsgibbs@outlook.com>
2023-03-08 15:24:42 -0500

    initial drop

---
+++ hello-world/src/app/strappin/strappin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-strappin',
+  templateUrl: './strappin.component.html',
+  styleUrls: ['./strappin.component.css'],
+})
+export class StrappinComponent {
+  init = `
+ Bonjour!
+      `;
+}
