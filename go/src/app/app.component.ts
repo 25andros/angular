5ba754c7ca23fd40344363fff53513c273498f21
andros25 <zsgibbs@outlook.com>
2023-04-22 17:35:11 -0400

    sidebar cleanup && mat-card refactor

--- go/src/app/app.component.ts
+++ go/src/app/app.component.ts
@@ -7,10 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'go';
-  
-  isOpen:boolean = true;
 
-  display(){
+  isOpen: boolean = true;
+  sidebarIcon: string = "chevron_right";
+
+  iconChange() {
+    if (this.isOpen == true) {
+      this.sidebarIcon = "chevron_right";
+    }
+    if (this.isOpen == false) {
+      this.sidebarIcon = "chevron_left";
+    }
+
+  }
+
+  display() {
 
   }
 }
